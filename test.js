86d0ce176137d297b0e756fcf4b31bfcb46a54e7
aburahan8721 <aburaihan8721@gmail.com>
2024-10-31 16:56:00 +0600

    auth update

--- test.js
+++
@@ -1,5 +0,0 @@
-const date = new Date();
-const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-console.log(lastMonth);
-const prevMonth = new Date(date.setMonth(lastMonth.getMonth() - 1));
-console.log(prevMonth);
